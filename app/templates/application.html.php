cd2f2c5dea4993f064651ed21fe5c8444bc84a75
Gbenga Ogunbule Supo <lebuogun@gmail.com>
2020-05-05 18:05:53 +0100

    Adding .htaccess and robots.txt to the public folder, adding application template to the template view folder

---
+++ app/templates/application.html.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+	<head>
+		<title><?=$title?></title>
+		<meta charset='utf-8'>
+		<link rel="stylesheet" type="text/css" href="/assets/css/login.css">
+		<meta name="robots" content="index, follow">
+	</head>
+	<body>
+		<?= $output ?>
+	</body>
+</html>
